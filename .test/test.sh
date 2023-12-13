b3bdadf8cff1f8d428e05c64b152d8a42c83d0f8
Simon Stier <52674635+simontaurus@users.noreply.github.com>
2023-12-13 10:31:36 +0100

    fix run multiple tests

--- .test/test.sh
+++ .test/test.sh
@@ -4,28 +4,24 @@ return_code=0
 for f in $(find /usr/local/apache2/htdocs -name '.htaccess')
 do
     echo "Processing file $f"
-    res=$(grep "##TESTv1" "$f")
-    if [ "$res" == "" ]; then
-        echo "No tests found"
-        continue
-    fi
-    #eval "parts=($IN)"
-    declare -a "parts=($( echo "$res" | sed 's/[][`~!@#$%^&*();<>,?\|{}=+]/\\&/g' ))"
+    grep "##TESTv1" "$f" | while read -r line ; do
+        #eval "parts=($IN)"
+        declare -a "parts=($( echo "$line" | sed 's/[][`~!@#$%^&*();<>,?\|{}=+]/\\&/g' ))"
 
-    curl="curl -sL -o /dev/null -w "%{url_effective}" --resolve www.w3id.org:80:127.0.0.1 www.w3id.org:80"
-    curl_cmd="${parts[1]}"
-    expected_result="${parts[2]}"
-    echo "TEST '$curl_cmd' => '$expected_result'"
-    
-    cmd="$curl$curl_cmd"
-    result=$(bash -c "$cmd")
-    if [ "$result" == "$expected_result" ]; then
-        echo "SUCCESS: Result is '$result'"
-    else
-        echo "ERROR  : Result is '$result'"
-        return_code=1
-    fi
+        curl="curl -sL -o /dev/null -w "%{url_effective}" --resolve www.w3id.org:80:127.0.0.1 www.w3id.org:80"
+        curl_cmd="${parts[1]}"
+        expected_result="${parts[2]}"
+        echo "TEST '$curl_cmd' => '$expected_result'"
+        
+        cmd="$curl$curl_cmd"
+        result=$(bash -c "$cmd")
+        if [ "$result" == "$expected_result" ]; then
+            echo "SUCCESS: Result is '$result'"
+        else
+            echo "ERROR  : Result is '$result'"
+            return_code=1
+        fi
+    done
 
 done
 exit $return_code
-
