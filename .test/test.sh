fcb41a5792af6ed8557f9d8293adbc9b5afa3861
Simon Stier <52674635+simontaurus@users.noreply.github.com>
2023-12-16 04:28:52 +0100

    allow multiple search paths, do nothing if SEARCH_PATH is empty

--- .test/test.sh
+++ .test/test.sh
@@ -5,48 +5,52 @@
 # default: no error
 return_code=0
 
-# search path can be restricted with env var $SEARCH_PATH
-for f in $(find /usr/local/apache2/htdocs$SEARCH_PATH -name '.htaccess')
-do
-    echo "Processing file $f"
+# SEARCH_PATH can provide a single ("/mypath") or multiple values ("/mypath1;/mypath2")
+IFS=';' read -ra paths <<< "$SEARCH_PATH"
+for path in "${paths[@]}"; do
 
-    tests_found=0
-
-    while IFS= read -r line
+    # search path can be restricted with env var $SEARCH_PATH
+    for f in $(find /usr/local/apache2/htdocs/$path -name '.htaccess')
     do
-        tests_found=$((tests_found + 1))
+        echo "Processing file $f"
 
-        # Security note: In gerneral it is not secure at all to eval user input
-        # (here and later in the curl command).
-        # Make sure to run the test within a encapsulated environment 
-        # and / or run only the test in your subpath.
-        declare -a "parts=($( echo "$line" | sed 's/##TESTv1//g' ))"  
+        tests_found=0
 
-        # note: we request only the effective url and create an alias with --resolve
-        # the subpath specified in the test gets appended to the request url
-        curl="curl -sL -o /dev/null -w "%{url_effective}" --resolve www.w3id.org:80:127.0.0.1 www.w3id.org:80"
-        curl_cmd="${parts[0]}"
-        expected_result="${parts[1]}"
+        while IFS= read -r line
+        do
+            tests_found=$((tests_found + 1))
 
-        echo "TEST '$curl_cmd' => '$expected_result'"
-        
-        cmd="$curl$curl_cmd"
-        result=$(bash -c "$cmd")
+            # Security note: In gerneral it is not secure at all to eval user input
+            # (here and later in the curl command).
+            # Make sure to run the test within a encapsulated environment 
+            # and / or run only the test in your subpath.
+            declare -a "parts=($( echo "$line" | sed 's/##TESTv1//g' ))"  
 
-        # compare the result url with the expected url
-        if [ "$result" == "$expected_result" ]; then
-            echo "SUCCESS: Result is '$result'"
-        else
-            echo "ERROR  : Result is '$result'"
-            return_code=1
+            # note: we request only the effective url and create an alias with --resolve
+            # the subpath specified in the test gets appended to the request url
+            curl="curl -sL -o /dev/null -w "%{url_effective}" --resolve www.w3id.org:80:127.0.0.1 www.w3id.org:80"
+            curl_cmd="${parts[0]}"
+            expected_result="${parts[1]}"
+
+            echo "TEST '$curl_cmd' => '$expected_result'"
+            
+            cmd="$curl$curl_cmd"
+            result=$(bash -c "$cmd")
+
+            # compare the result url with the expected url
+            if [ "$result" == "$expected_result" ]; then
+                echo "SUCCESS: Result is '$result'"
+            else
+                echo "ERROR  : Result is '$result'"
+                return_code=1
+            fi
+        done < <(grep "##TESTv1" "$f")
+
+        if [ $tests_found == 0 ]; then
+            echo "No tests found"
         fi
-    done < <(grep "##TESTv1" "$f")
 
-    if [ $tests_found == 0 ]; then
-        echo "No tests found"
-    fi
+done
 
 done
 exit $return_code
-
-
